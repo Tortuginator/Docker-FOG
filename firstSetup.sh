eb914c2b4ba52d1c132b02cf6dd67d29f9989e2d
Felix <felix.friedberger@gmail.com>
2018-04-08 19:11:24 +0200

    fixes

--- firstSetup.sh
+++ firstSetup.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 ## Database user and password must be included in the variables DB_USER, DB_PASS and DB_NAME
-
 /etc/init.d/rpcbind start
 /etc/init.d/vsftpd start
 /etc/init.d/mysql start
-/etc/init.d/nfs-kernel-server start
 /usr/sbin/apachectl -D FOREGROUND
-
 #patch ipservice
 echo "mountd 34463/tcp" >> /etc/services
 echo "mountd 34463/udp" >> /etc/services
--- startInstance.sh
+++ startInstance.sh
@@ -5,6 +5,7 @@ python /tmp/fixChain.py
 /etc/init.d/rpcbind start
 /etc/init.d/vsftpd start
 /etc/init.d/mysql start
+in.tftpd --listen --address 0.0.0.0:69 -s /tftpboot
 mysqld &
 /usr/sbin/apachectl -D FOREGROUND
 #If the commands above do not trigger the service start for some reason
